1dfa4b62d570ed4ecaae6cb43099aefc233a15f0
kakaana <140449084+kakaana@users.noreply.github.com>
2024-11-11 13:36:21 -0300

    ,

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,24 @@
 criaCartao(
-    'Geografia'
-    'qual é a capital do Brasil?'
+    'Geografia',
+    'qual é a capital do Brasil?',
     'a capital do Brasil é Brasilía'
 )
 
 criaCartao(
-    'Matematica'
-    'quanto é 4x+6?'
+    'Matematica',
+    'quanto é 4x+6?',
     'x=1,5'
 )
 
 criaCartao(
-    'curiosidade'
-    'O que é o melão?'
+    'curiosidade',
+    'O que é o melão?',
     'uma fruta'
     
 )
 
 criaCartao(
-    'Lingua inglesa'
-    'Como se diz olá em Inglês?'
+    'Lingua inglesa',
+    'Como se diz olá em Inglês?',
     'Olá em inglês e hello'
 )
